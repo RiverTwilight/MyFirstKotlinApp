c718fab91d70564717901df5e210ce5021112d8d
RiverTwilight <wrj2014@126.com>
2021-05-23 14:48:32 +0800

    add function

--- app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.myfirstkotlinapp
 
 import android.os.Bundle
-import android.content.Intent
 import android.support.wearable.activity.WearableActivity
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import kotlin.random.Random
 
 const val EXTRA_MESSAGE = "com.example.MyFirstKotlinApp.MESSAGE"
+
+fun rand(start: Int, end: Int): Int {
+    require(!(start > end || end - start + 1 > Int.MAX_VALUE)) { "Illegal Argument" }
+    return Random(System.nanoTime()).nextInt(end - start + 1) + start
+}
 
 class MainActivity : WearableActivity() {
 
@@ -21,9 +26,11 @@ class MainActivity : WearableActivity() {
 
     /** Called when the user taps the Send button */
     fun sendMessage(view: View) {
-        val maxValue = findViewById<EditText>(R.id.maxValue);
+        val maxValue = findViewById<EditText>(R.id.maxValue).text.toString().toInt();
+        val minValue = findViewById<EditText>(R.id.minValue).text.toString().toInt()
+
         var btn = findViewById<Button>(R.id.start).apply {
-            text = maxValue.text.toString()
+            text = rand(minValue, maxValue).toString()
         }
     }
 }
