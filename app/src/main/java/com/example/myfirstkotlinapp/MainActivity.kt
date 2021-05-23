8f2fb2f05868b19e3c3f905a4cb7f2ca4059c347
RiverTwilight <wrj2014@126.com>
2021-05-23 09:33:13 +0800

    ui

--- app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstkotlinapp/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.myfirstkotlinapp
 
 import android.os.Bundle
+import android.content.Intent
 import android.support.wearable.activity.WearableActivity
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.MyFirstKotlinApp.MESSAGE"
 
 class MainActivity : WearableActivity() {
 
@@ -12,10 +17,13 @@ class MainActivity : WearableActivity() {
 
         // Enables Always-on
         setAmbientEnabled()
+    }
 
-        /** Called when the user taps the Send button */
-        fun sendMessage(view: View) {
-            // Do something in response to button
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val maxValue = findViewById<EditText>(R.id.maxValue);
+        var btn = findViewById<Button>(R.id.start).apply {
+            text = maxValue.text.toString()
         }
     }
 }
